3d937f7c2cae2de01df7bf2f4d2436f7c7995775
Adrian Diaconita <adi2003diaconita@gmail.com>
2024-11-16 14:44:01 +0200

    Added Spring Boot Configuration files

---
+++ teatru-reservations/src/test/java/com/example/teatru_reservations/TeatruReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teatru_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatruReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
